6e61662d2503f44d74e8cf28b4cedccb78009f01
yudaeon <124870889+yudaeon@users.noreply.github.com>
2023-06-10 02:03:10 +0900

    [Bronze II] Title: 수 정렬하기, Time: 352 ms, Memory: 22584 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n-1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+            for (int val : arr){
+                System.out.println(val);
+            }
+        }
+    }
