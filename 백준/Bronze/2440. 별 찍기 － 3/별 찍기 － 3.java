f646b9aef884ff314228167e105da68aaab221ea
yudaeon <124870889+yudaeon@users.noreply.github.com>
2023-06-10 16:13:27 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 316 ms, Memory: 18136 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int star = sc.nextInt();
+
+        for (int i = 0; i < star; i++) {
+            for (int j = i+1; j <=star; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
