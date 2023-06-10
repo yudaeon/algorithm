5030a5b04917a5b59e6d54b3a62a47c5ad5ae4c7
yudaeon <124870889+yudaeon@users.noreply.github.com>
2023-06-10 16:14:49 +0900

    [Bronze V] Title: 합, Time: 204 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in );
+        int num = sc.nextInt();
+        int sum=0;
+
+        //1부터 n까지 합 구하기
+        for (int i = 1; i <=num; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
