d054e515629ce4daff0e4965eb1cfa60584fc9a8
yudaeon <124870889+yudaeon@users.noreply.github.com>
2023-06-10 17:07:34 +0900

    [Bronze III] Title: 오븐 시계, Time: 228 ms, Memory: 18376 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+   
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        int c = sc.nextInt();
+        int a = h * 60 + m + c;
+        h = a/60;
+        m = a%60;
+        if (h>=24){
+            h = h-24;
+        }
+        System.out.println(h + " " + m);
+    }
+}
