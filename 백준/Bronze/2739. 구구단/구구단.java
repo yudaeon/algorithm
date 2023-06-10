f645fd4351f9ea061565bd4a2b8927dd8c4bb6bb
yudaeon <124870889+yudaeon@users.noreply.github.com>
2023-06-10 16:15:16 +0900

    [Bronze V] Title: 구구단, Time: 236 ms, Memory: 18508 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main { //반복문) 구구단 출력
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for (int i = 1; i < 10; i++) {
+            System.out.println(a + " * " + i + " = " +(a*i));
+        }
+    }
+}
